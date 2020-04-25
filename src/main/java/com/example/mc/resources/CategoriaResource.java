5041242bd135c9c729d919610fb13745cdf82971
Marcus Furtuna <marcus.furtuna04@gmail.com>
2020-04-24 22:15:29 -0300

    Testendo o REST

---
+++ src/main/java/com/example/mc/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package com.example.mc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+			
+		return "RES está funcionando";
+	}
+	
+	
+	
+	
+}
