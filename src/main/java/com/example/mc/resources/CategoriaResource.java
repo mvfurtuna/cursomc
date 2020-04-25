6c41870df572b24f490ba1ded91bca9892de4331
Marcus Furtuna <marcus.furtuna04@gmail.com>
2020-04-24 22:33:45 -0300

    Testando a classe domain Categoria

--- src/main/java/com/example/mc/resources/CategoriaResource.java
+++ src/main/java/com/example/mc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.example.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,9 +15,19 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 			
-		return "RES está funcionando";
+
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listCat = new ArrayList<>();
+		
+		listCat.add(cat1);
+		listCat.add(cat2);
+		
+		
+		return listCat;
 	}
 	
 	
